643f82b74bbc3e00d5f45c3ad939e2b6c3d88b87
ankihg <hagelin.annika@gmail.com>
2016-05-19 15:00:52 -0700

    some d3

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 console.log('hi from index');
+
+// d3.selectAll("circle").style("color", function() {
+//   return "hsl(" + Math.random() * 360 + ",100%,50%)";
+// });
+
+d3.select("svg")
+  .append("circle")
+  .attr("cx", 20)
+  .attr("cy", 20)
+  .attr("r", 10);
+
+d3.select("#root")
+  .attr("fill", "green");
+
+console.log(d3.select('#main'));
