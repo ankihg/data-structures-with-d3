2c480302a790678595c333123f21737aa49659ad
ankihg <hagelin.annika@gmail.com>
2016-05-21 19:49:17 -0700

    text added

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,36 @@
+'use strict';
 console.log('hi from index');
 
-// d3.selectAll("circle").style("color", function() {
-//   return "hsl(" + Math.random() * 360 + ",100%,50%)";
-// });
+// d3.select("svg")
+//   .append("circle")
+//   .attr("cx", 20)
+//   .attr("cy", 20)
+//   .attr("r", 10);
+//
+// d3.select("#root")
+//   .attr("fill", "green");
 
-d3.select("svg")
-  .append("circle")
-  .attr("cx", 20)
-  .attr("cy", 20)
-  .attr("r", 10);
+var linkedList = new Node(null);
+linkedList.append(5);
+linkedList.append(7);
+linkedList.append(9);
+linkedList.append(10);
+processLinkedList(linkedList.next, 10);
 
-d3.select("#root")
-  .attr("fill", "green");
-
-console.log(d3.select('#main'));
+function processLinkedList(linkedList, x) {
+  var nodeDiv = d3.select("svg").append("g");
+  nodeDiv
+    .append("circle")
+    .attr('fill', 'green')
+    .attr("cx", x)
+    .attr("cy", 20)
+    .attr("r", 10);
+  nodeDiv
+    .append("text")
+    .attr("x", x+10)
+    .attr("y", 50)
+    .attr("dx", function(d){return -20})
+    .style('color', 'green')
+    .text(() => linkedList.val);
+  if (linkedList.next) processLinkedList(linkedList.next, x+50);
+}
